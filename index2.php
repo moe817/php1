f3d6a244de3762d8bf30a0235277a1af8a978617
moe817 <moe.sasaki.0817@gmail.com>
2022-04-09 13:30:52 +0900

    次の課題を試して見る

---
+++ index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action = "index2.php" method = "POST">
+<input type = "text" name ="arraysearch"><br/>
+<input type = "submit" value ="送信">
+</form>
+<?php
+$fruits = ['apple', 'orange', 'strawberry'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    foreach($fruits as $fruit){
+        if($_POST['arraysearch'] === $fruit){
+            echo $fruit;
+        }
+    }
+}
+?>
+    
+</body>
+</html>
