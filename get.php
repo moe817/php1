19fe58a5d02abe91691a4c23e94bd65c6152d8de
moe817 <moe.sasaki.0817@gmail.com>
2022-04-09 09:30:02 +0900

    追加
    postのところをgetに変更

--- get.php
+++ get.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    if ($_POST['postcomment'] === '東京') {
+    if ($_GET['getcomment'] === '東京') {
         echo '正解';
     } else {
         echo '不正解';
